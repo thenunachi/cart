9d98cbd4e0adff58cdcdf51f953ac8c35152b412
ThenuKalyan <thenammainachiyappan@ThenuKalyans-MacBook-Air.local>
2025-11-04 09:36:57 -0800

    Fix repo structure, remove duplicate folder

--- gitRepo/cart/productList/backend/venv/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "supersecretkey"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///users.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = "jwt-secret-key"  # change this in production
